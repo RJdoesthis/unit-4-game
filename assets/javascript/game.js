56ef2b88f640c5f20d085efd737e4c023c77be96
Rodney Julien <RodneyJulien@gmail.com>
2019-10-13 18:41:57 -0400

    updated game
    updated game

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+// Javascript for game
+
+button = document.getElementsByClassName("btn");
+btn.addeventlistener("click", alertme);
+
+function alertMe() {
+    alert('The button has been clicked!');
+
+}
