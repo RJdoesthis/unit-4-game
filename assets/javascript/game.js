24a727eca149c1b2f06d585f34c48cac4286f326
Rodney Julien <RodneyJulien@gmail.com>
2019-10-13 23:27:18 -0400

    updated with random number for button
    updated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,43 @@
-// Javascript for game
+$(document).ready(function () {
+    function randomNumber() {
+        let number = Math.floor((Math.random() * 101) + 19);
+        console.log(number);
+        $('#RandomNumber').text(number);
+        //the above code will generate the random number and place it in the div
+    }
+    //creating a random number for each button
+    let num1 = Math.floor(Math.random() * 11 + 1)
+    let num2 = Math.floor(Math.random() * 11 + 1)
+    let num3 = Math.floor(Math.random() * 11 + 1)
+    let num4 = Math.floor(Math.random() * 11 + 1)
 
-button = document.getElementsByClassName("btn");
-btn.addeventlistener("click", alertme);
+    //counting wins and losss
+    let wins = 0;
+    let losses = 0;
 
-function alertMe() {
-    alert('The button has been clicked!');
+    //below are the click events for each button type
+    $('#one').on('click', function () {
+        let uservalue = num1;
+        console.log(num1);
+    })
 
-}
+    $('#two').on('click', function () {
+        let uservalue = num2;
+        console.log(num2);
+    })
+
+    $('#three').on('click', function () {
+        let uservalue = num3;
+        console.log(num3);
+
+    })
+
+    $('#four').on('click', function () {
+        let uservalue = num4;
+        console.log(num4);
+
+    })
+
+    randomNumber();
+})
 
